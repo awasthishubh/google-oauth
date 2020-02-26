9cf95ad929eb264d88b344d94eca542965e36a79
Shubham Awasthi <aw.shubh@gmail.com>
2020-02-26 17:17:41 +0530

    Func: Get Token from Access code

--- module/Code2token.hs
+++ module/Code2token.hs
@@ -1,4 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Code2token where
+	import qualified Data.ByteString.Lazy.Char8 as L8
+	import           Network.HTTP.Client        (defaultManagerSettings, newManager)
+	import           Network.HTTP.Simple
+	import			 Network.HTTP.Client.TLS
+	import           Data.Aeson
+	data Auth = Auth String String String String String
+	instance ToJSON Auth where
+		toJSON (Auth client_id client_secret grant_type code redirect_uri) = object
+				[
+					"client_id" .= client_id,
+					"client_secret" .= client_secret,
+					"grant_type" .= grant_type,
+					"code" .= code,
+					"redirect_uri" .= redirect_uri
+				]
+	
+					
+	getToken :: String->String->String->String->String->IO String
+	getToken client_id client_secret grant_type code redirect_uri = do
+		let auth =Auth client_id client_secret grant_type code redirect_uri  
+		manager <- newManager tlsManagerSettings
 
-token="Token"
+		let request 
+			= setRequestMethod "POST"
+			$ setRequestBodyJSON auth
+			$ setRequestHeaders [
+				("user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36")
+			]
+			$ setRequestSecure True
+			$ setRequestManager manager "https://accounts.google.com/o/oauth2/token"
+		response <- httpLBS request
+		return $ L8.unpack $ getResponseBody response
--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
 import Control.Monad.IO.Class
-import Code2token
+import qualified Code2token as C
 import Token2info
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
 
-main = scotty 3000 $ do
-    get "/" $ do
-        liftIO $ putStrLn $ "Hello World!!"
-        text $L.pack info
+main::IO ()
+main=do
+    putStrLn "From Main"
+    let client_id="698827909931-28n38gm9dgg7gjaainvo77tu90kchcl0.apps.googleusercontent.com" 
+        client_secret="vwLCiXN99on2Fl6cako-niOS" 
+        grant_type="authorization_code" 
+        code="4/wwECwhwV-m7Zf6BmQ26ILA_GrjVdrNn-0nK43UqtyEQ7TFDCb2J6jKrO9Nvi0doT1r9EXglVYEWyEhfibGh0Vys" 
+        redirect_uri="http://localhost:3000/callback"
+    res<-C.getToken client_id client_secret grant_type code redirect_uri
+    putStrLn res
