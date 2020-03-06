c54e83a31f0420d7d78d6531c929e0cd9bfc954f
Shubham Awasthi <aw.shubh@gmail.com>
2020-03-06 20:50:20 +0530

    Send token response to main

--- module/Code2token.hs
+++ module/Code2token.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Code2token where
-	import qualified Data.ByteString.Lazy.Char8 as L8
+	-- import qualified Data.ByteString.Lazy.Char8 as L8
 	import           Network.HTTP.Client        (defaultManagerSettings, newManager)
 	import           Network.HTTP.Simple
 	import			 Network.HTTP.Client.TLS
-	import           Data.Aeson
+	import           Data.Aeson 
+	import Data.Aeson.Types
+	-- import Data.Aeson.Types(Parser(..))
+
+	data Token = Token String String deriving(Show)
+
 	data Auth = Auth String String String String String
 	instance ToJSON Auth where
 		toJSON (Auth client_id client_secret grant_type code redirect_uri) = object
@@ -16,8 +21,12 @@ module Code2token where
 					"redirect_uri" .= redirect_uri
 				]
 	
+	getPar :: Maybe String -> String
+	getPar Nothing = ""
+	getPar (Just a) = a
+	getPar _ = ""
 					
-	getToken :: String->String->String->String->String->IO String
+	getToken :: String->String->String->String->String->IO Token
 	getToken client_id client_secret grant_type code redirect_uri = do
 		let auth =Auth client_id client_secret grant_type code redirect_uri  
 		manager <- newManager tlsManagerSettings
@@ -30,5 +39,10 @@ module Code2token where
 			]
 			$ setRequestSecure True
 			$ setRequestManager manager "https://accounts.google.com/o/oauth2/token"
-		response <- httpLBS request
-		return $ L8.unpack $ getResponseBody response
+		-- response <- getResponseBody <$> httpJSON request :: IO (Response Object)
+		-- -- let a = fromJust $ parseMaybe $(getResponseBody response) .: "error" :: Parser String
+		response <- getResponseBody <$> httpJSON request
+		let refresh_token =  getPar $ parseMaybe (.: "refresh_token") response :: String
+		let access_token =  getPar $ parseMaybe (.: "access_token") response :: String
+		let token=Token refresh_token access_token::Token
+		return $ token
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 import Web.Scotty
 import Control.Monad.IO.Class
 import qualified Code2token as C
+import qualified Token2info as T
 import Token2info
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
@@ -12,7 +13,15 @@ main=do
     let client_id="698827909931-28n38gm9dgg7gjaainvo77tu90kchcl0.apps.googleusercontent.com" 
         client_secret="vwLCiXN99on2Fl6cako-niOS" 
         grant_type="authorization_code" 
-        code="4/wwECwhwV-m7Zf6BmQ26ILA_GrjVdrNn-0nK43UqtyEQ7TFDCb2J6jKrO9Nvi0doT1r9EXglVYEWyEhfibGh0Vys" 
+        code="4/xQHTqltB6snh-cRcTGIj-QBMacfLnRFbGADvhxp7KlwwLa3kmTe3OrSPTI3RXHgYnbf-d3rXlZlrjxTWgKhHzhk" 
         redirect_uri="http://localhost:3000/callback"
-    res<-C.getToken client_id client_secret grant_type code redirect_uri
-    putStrLn res
+    tokens<-C.getToken client_id client_secret grant_type code redirect_uri
+
+    putStrLn $ show tokens
+-- import qualified Data.ByteString.Lazy.Char8 as BS
+-- import Data.Aeson
+-- main::IO ()
+-- main=do
+--     let foo = BS.pack "{\"Name\" : {\"Name\" : \"Stitch\", \"Age\" : 3, \"Friend\": \"Lilo\"}, \"Age\" : 3, \"Friend\": \"Lilo\"}"
+--     let res = decode foo::Maybe Object
+--     putStrLn.show $ res
