62ca51567564c4df91c731388b2e77a5d61888c7
Shubham Awasthi <aw.shubh@gmail.com>
2020-03-11 13:15:53 +0530

    Token2Info: Completed request

--- module/Token2info.hs
+++ module/Token2info.hs
@@ -1,4 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Token2info where
+	-- import qualified Data.ByteString.Lazy.Char8 as L8
+	import           Network.HTTP.Client        (defaultManagerSettings, newManager)
+	import           Network.HTTP.Simple
+	import			 Network.HTTP.Client.TLS
+	import Data.Aeson.Types
+	import Types
+	import qualified Data.ByteString.Char8 as K
 
-info::String
-info="Info"
+	getInfo :: String -> IO Object
+	getInfo access_code = do
+		manager <- newManager tlsManagerSettings
+		let request 
+			= setRequestMethod "GET"
+			$ setRequestHeaders [
+				("user-agent" , "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"),
+				("Authorization" , K.pack $ "Bearer "++access_code)
+			]
+			$ setRequestSecure True
+			$ setRequestManager manager "https://www.googleapis.com/oauth2/v2/userinfo"
+		response <- getResponseBody <$> httpJSON request :: IO Object
+		return response
--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,7 @@
 import Web.Scotty
 import Control.Monad.IO.Class
 import qualified Code2token as C
-import qualified Token2info as T
-import Token2info
+import Token2info as TI
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
 
@@ -13,11 +12,14 @@ main=do
     let client_id="698827909931-28n38gm9dgg7gjaainvo77tu90kchcl0.apps.googleusercontent.com" 
         client_secret="vwLCiXN99on2Fl6cako-niOS" 
         grant_type="authorization_code" 
-        code="4/xQHTqltB6snh-cRcTGIj-QBMacfLnRFbGADvhxp7KlwwLa3kmTe3OrSPTI3RXHgYnbf-d3rXlZlrjxTWgKhHzhk" 
+        code="4/xQEJc9xnAHRhoUUil-RTKzOgfL1vppEkaEBWnoHMq4rWj27ku4PlA8SQgYjVTGrOgR2NzZZl_vfrOl4t1ukVqT0" 
         redirect_uri="http://localhost:3000/callback"
-    tokens<-C.getToken client_id client_secret grant_type code redirect_uri
+        access_code="ya29.a0Adw1xeXck4Q8qt0Z0S_UyBN14dFgi7hk6tnbOUS-_zJXKD8iF6uJBrfaeHQzHJ0_nOTnmG2miBTqtCz42TKfrrAzWdYt81cQKAuYGw_9UQSn5Ae-2nx_eWVzxFIsvyIBVIPLlwlNQ7rPLdg9Ah0w0sHAFnq_r_UCGdo"
+    userInfo <- TI.getInfo access_code
+    putStrLn $ show userInfo
+    -- tokens<-C.getToken client_id client_secret grant_type code redirect_uri
 
-    putStrLn $ show tokens
+    -- putStrLn $ show tokens
 -- import qualified Data.ByteString.Lazy.Char8 as BS
 -- import Data.Aeson
 -- main::IO ()
